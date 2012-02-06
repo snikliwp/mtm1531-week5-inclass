f8d963f512c8367ffda7bbb3f22d2e06049c2b54
snikliwp <wilk0146@algonquinlive.com>
2012-02-06 12:05:18 -0500

    initial load

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+require_once '../includes/db.php';
+var_dump($db);
+?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Untitled Document</title>
+	<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+</body>
+</html>
